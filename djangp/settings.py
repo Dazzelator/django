a9e7f7d90f6ecb2927edd2b769bc2b1c7aa6fbed
DarrenCoppinger <darrencoppinger@gmail.com>
2020-04-21 12:57:26 +0000

    Implemented environment specific values for DEBUG and DATABASES

--- djangp/settings.py
+++ djangp/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 import dj_database_url
+if os.environ.get('DEVELOPMENT'):
+    development = True
+else:
+    development = False
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -24,10 +28,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$wnk69#*a7urf3ra+4ndck&xr6u1#_e)ex%x%sj87!^2y161(3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+
+DEBUG = development
 
 # ALLOWED_HOSTS = ['dc-django-todo.herokuapp.com']
-ALLOWED_HOSTS = [os.environ.get('HOSTNAME')]
+ALLOWED_HOSTS = [ 'localhost', os.environ.get('HOSTNAME')]
 
 
 
@@ -77,15 +83,18 @@ WSGI_APPLICATION = 'djangp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+if development:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+else:
+    DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
 
 
-DATABASES = {'default': dj_database_url.parse("postgres://krpsvvqnlezkoq:0e8ee07395877f9e3f84d84c6ea98a63697a8cbb237f2f86e210bdd644fc0afc@ec2-54-228-250-82.eu-west-1.compute.amazonaws.com:5432/d28umf6kqqpa51")}
+# DATABASES = {'default': dj_database_url.parse("postgres://krpsvvqnlezkoq:0e8ee07395877f9e3f84d84c6ea98a63697a8cbb237f2f86e210bdd644fc0afc@ec2-54-228-250-82.eu-west-1.compute.amazonaws.com:5432/d28umf6kqqpa51")}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
